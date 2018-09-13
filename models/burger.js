e3ff1ae11f0ea60eb5a02d7f016b5784b5d7b319
Elephant_Man <grogan.charlie@gmail.com>
2018-09-12 22:13:44 -0500

    orm paths and functions started
    -needs tweaking

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,10 @@
-require("./config/orm.js");
+const orm = require("./config/orm.js");
+
+// Find all the burgers unserved.
+orm.selectAll("burger_name", "burger_served");
+
+// Find a burger in the burgerHistory table by an burger_served false.
+orm.insertOne("burgerHistory", "burger_name", false);
+
+//  update burgerHistory table
+orm.updateOne("buyer_name", "buyer_id", "buyers", "pets");
